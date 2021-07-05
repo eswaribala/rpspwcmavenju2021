f17124ac6555237d1bdba6f2c7eb39a9d5f4d915
eswaribala <Parameswaribala@gmail.com>
2021-07-05 17:14:30 +0530

    Update forloopdemo.sh

--- forloopdemo.sh
+++ forloopdemo.sh
@@ -4,10 +4,20 @@ read movies
 
 movienames=()
 for i in $(seq 1 $movies)
- do
-   echo "Enter movie name"
-   read movieName
-   movienames[$i]=$movieName
- done
- #display movie names
- echo "${movienames[@]}"
+do
+  echo "Enter movie name"
+  read movieName
+  movienames[$i]=$movieName
+done
+#display movie names
+echo "${movienames[@]}"
+#read data from array
+pos=1
+length=${#movienames[@]}
+echo $length
+while [ $pos -le $length ]
+do
+  echo "${movienames[$pos]}"
+   ((pos++))
+   echo "$pos"
+done
